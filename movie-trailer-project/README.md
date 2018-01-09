a0c7a064c1222a6c7c71dca54af608da3c9c83c2
Bawalters6 <30614469+Bawalters6@users.noreply.github.com>
2018-01-08 17:01:42 -0700

    Add files via upload

---
+++ movie-trailer-project/README.md
@@ -0,0 +1,12 @@
+# **Movie Trailer Webpage** #
+
+## **Discription** ##
+This is a webpage that shows my favorite movies, along with the movie's trailer.
+
+## **Usage** ##
+To use this webpage, all you need to do is scroll through the movies. Once you find a movie you would like to see the trailer of, just click on the movie image picture and the movie trailer will pop up in a new window.
+
+### **Additional Information** ###
+_This is just a list of some of my own personal favorite movies, there is no need or want for collaboration on this project, just a fun webpage to show some of my favorite movies and their trailers._
+
+
---
+++ movie-trailer-project/entertainment_center.py
@@ -0,0 +1,212 @@
+import fresh_tomatoes
+import media
+
+role_models = media.Movie("Role Models", "After salesmen Danny (Paul Rudd) and"
+                          " Wheeler (Seann William Scott) trash a company"
+                          " truck, the court gives them a choice: jail time or"
+                          " community service in a mentoring program. Thinking"
+                          " to take the easy way out, the two overgrown"
+                          " adolescents find themselves paired with a teenager"
+                          " (Christopher Mintz-Plasse), who is experiencing"
+                          " the pangs of first love, and a foul-mouthed"
+                          " fifth-grader (Bobb'e J. Thompson), who needs an"
+                          " attitude adjustment.",
+                          "http://www.gstatic.com/tv/thumb/movieposters/182388/p182388_p_v8_af.jpg",
+                          "https://youtu.be/yZGYswlbU8w")
+
+fight_club = media.Movie("Fight Club", "A depressed man (Edward Norton)"
+                         " suffering from insomnia meets a strange soap"
+                         " salesman named Tyler Durden (Brad Pitt) and soon"
+                         " finds himself living in his squalid house after his"
+                         " perfect apartment is destroyed. The two bored men"
+                         " form an underground club with strict rules and"
+                         " fight other men who are fed up with their mundane"
+                         " lives. Their perfect partnership frays when Marla"
+                         " (Helena Bonham Carter), a fellow support group"
+                         " crasher, attracts Tyler's attention.",
+                         "http://cdn.playbuzz.com/cdn/a99cfa7f-1267-42ca-9ae3-516d37c42005/fb69c47f-a441-4abd-932c-73503410383c.jpg",
+                         "https://youtu.be/D3Yw9Yc1YmY")
+
+slc_punk = media.Movie("SLC Punk!", "To grow up in America is to, at one point"
+                       " or another, rebel against the world around you. If"
+                       " you're Stevo and you live in Reagan-era Salt Lake"
+                       " City, then, well...you just have to rebel a little"
+                       " bit harder. To this recent grad, the world is his"
+                       " enemy, anarchy his religion, and the Ramones, The"
+                       " Specials and the Dead Kennedys ",
+                       "https://i.jeded.com/i/slc-punk.15726.jpg",
+                       "https://youtu.be/WlYj6uhs22U")
+
+requiem_for_a_dream = media.Movie("Requiem For A Dream", "Imaginatively evoking"
+                                  " the inner landscape of human beings longing"
+                                  " to connect, to love and feel loved, the"
+                                  " film is a parable of happiness gloriously"
+                                  " found and tragically lost. 'Requiem for a"
+                                  " Dream' tells parallel stories that are"
+                                  " linked by the relationship between the"
+                                  " lonely, widowed Sara Goldfarb and her"
+                                  " sweet but aimless son, Harry. The plump"
+                                  " Sara, galvanized by the prospect of"
+                                  " appearing on a TV game show, has started"
+                                  " on a dangerous diet regimen to beautify"
+                                  " herself for a national audience.",
+                                  "https://s-media-cache-ak0.pinimg.com/originals/a5/ed/6f/a5ed6f32d0606542351902fb3871524c.jpg",
+                                  "https://youtu.be/-YDk89e1miY")
+
+dirty_rotten_scoundrels = media.Movie("Dirty Rotten Scoundrels", "Con artist"
+                                      " Lawrence Jamieson (Michael Caine) is a"
+                                      " longtime resident of a luxurious"
+                                      " coastal resort, where he enjoys the"
+                                      " fruits of his deceptions -- that is,"
+                                      " until a competitor, Freddy Benson"
+                                      " (Steve Martin), shows up. When the new"
+                                      " guy's lowbrow tactics impinge on his"
+                                      " own work, Jamieson resolves to get rid"
+                                      " of him. Confident of his own"
+                                      " duplicitous talents, Jamieson"
+                                      " challenges Benson to a winner-takes-all"
+                                      " competition: whoever swindles their"
+                                      " latest mark first can stay, while the"
+                                      " other must leave town.",
+                                      "http://static.rogerebert.com/uploads/movie/movie_poster/dirty-rotten-scoundrels-1988/large_z1AOwsRQCtGqn0VYEK3RQ5bYcxm.jpg",
+                                      "https://youtu.be/0ke-v0e3Cd4")
+
+good_will_hunting = media.Movie("Good Will Hunting", "Will Hunting (Matt Damon)"
+                                " has a genius-level IQ but chooses to work as"
+                                " a janitor at MIT. When he solves a difficult"
+                                " graduate-level math problem, his talents are"
+                                " discovered by Professor Gerald Lambeau"
+                                " (Stellan Skarsgard), who decides to help the"
+                                " misguided youth reach his potential. When"
+                                " Will is arrested for attacking a police"
+                                " officer, Professor Lambeau makes a deal to"
+                                " get leniency for him if he will get treatment"
+                                " from therapist Sean Maguire"
+                                " (Robin Williams).",
+                                "http://www.impawards.com/1997/posters/good_will_hunting.jpg",
+                                "https://youtu.be/nH9LZOXBMUE")
+
+i_am_sam = media.Movie("I Am Sam", "'I Am Sam' is the compelling story of Sam"
+                       " Dawson (Sean Penn), a mentally-challenged father"
+                       " raising his daughter Lucy (Dakota Fanning) with the"
+                       " help of a unique group of friends. As Lucy turns"
+                       " seven and begins to intellectually surpass her"
+                       " father, their close bond is threatened when their"
+                       " unconventional living arrangement comes to the"
+                       " attention of a social worker who wants Lucy placed in"
+                       " foster care.",
+                       "http://www.impawards.com/2001/posters/i_am_sam_ver1.jpg",
+                       "https://youtu.be/z_AguDqCBvo")
+
+death_proof = media.Movie("Death Proof", "Stuntman Mike (Kurt Russell) is a"
+                          " professional body double who likes to take"
+                          " unsuspecting women for deadly drives in his free"
+                          " time. He has doctored his car for maximum impact;"
+                          " when Mike purposely causes wrecks, the bodies pile"
+                          " up while he walks away with barely a scratch. The"
+                          " insane Mike may be in over his head, though, when"
+                          " he targets a tough group of female friends,"
+                          " including real-life stuntwoman Zoe Bell, who plays"
+                          " herself.",
+                          "https://s-media-cache-ak0.pinimg.com/originals/c3/bf/f3/c3bff3eec6f1b0e8f1437972f80d5493.jpg",
+                          "https://youtu.be/7mICGcg5-pM")
+                          
+the_grand_budapest_hotel = media.Movie("The Grand Budapest Hotel", "In the"
+                                       " 1930s, the Grand Budapest Hotel is a"
+                                       " popular European ski resort, presided"
+                                       " over by concierge Gustave H. (Ralph"
+                                       " Fiennes). Zero, a junior lobby boy,"
+                                       " becomes Gustave's friend and protege."
+                                       " Gustave prides himself on providing"
+                                       " first-class service to the hotel's"
+                                       " guests, including satisfying the"
+                                       " sexual needs of the many elderly"
+                                       " women who stay there. When one of"
+                                       " Gustave's lovers dies mysteriously,"
+                                       " Gustave finds himself the recipient"
+                                       " of a priceless painting and the chief"
+                                       " suspect in her murder.",
+                                       "http://www.impawards.com/2014/posters/grand_budapest_hotel.jpg",
+                                       "https://youtu.be/1Fg5iWmQjwk")
+
+the_graduate = media.Movie("The Graduate", "Benjamin Braddock (Dustin Hoffman)"
+                           " has just finished college and, back at his"
+                           " parents' house, he's trying to avoid the one"
+                           " question everyone keeps asking: What does he want"
+                           " to do with his life? An unexpected diversion"
+                           " crops up when he is seduced by Mrs. Robinson"
+                           " (Anne Bancroft), a bored housewife and friend of"
+                           " his parents. But what begins as a fun tryst turns"
+                           " complicated when Benjamin falls for the one woman"
+                           " Mrs. Robinson demanded he stay away from, her"
+                           " daughter, Elaine (Katharine Ross).",
+                           "https://images-na.ssl-images-amazon.com/images/I/51jlxsaTwLL._AC_UL320_SR236,320_.jpg",
+                           "https://youtu.be/XxJDOkr_UhE")
+
+rocky_iv = media.Movie("Rocky IV", "After reclaiming the boxing championship"
+                       " title, Rocky Balboa (Sylvester Stallone) plans to"
+                       " retire and live with his wife, Adrian (Talia Shire)."
+                       " However, during an exhibition match, Rocky's friend"
+                       " Apollo Creed (Carl Weathers) is mercilessly beaten to"
+                       " death by hulking Russian newcomer Ivan Drago (Dolph"
+                       " Lundgren). Rocky vows payback against Drago and flies"
+                       " to Russia to train for a Christmas Day fight. Despite"
+                       " their different training methods, Rocky and Drago both"
+                       " wage a long and intense match.",
+                       "https://upload.wikimedia.org/wikipedia/en/9/95/Rocky_IV.jpg",
+                       "https://youtu.be/mIE5HYkzvV0")
+
+memento = media.Movie("Memento", "Leonard (Guy Pearce) is tracking down the man"
+                      " who raped and murdered his wife. The difficulty,"
+                      " however, of locating his wife's killer is compounded by"
+                      " the fact that he suffers from a rare, untreatable form"
+                      " of memory loss. Although he can recall details of life"
+                      " before his accident, Leonard cannot remember what"
+                      " happened fifteen minutes ago, where he's going, or"
+                      " why.", 
+                      "https://kadreview.files.wordpress.com/2013/03/memento.jpg",
+                      "https://youtu.be/UFuFFdK7i44")
+
+the_big_lebowski = media.Movie("The Big Lebowski", "Jeff Bridges plays Jeff"
+                               " Lebowski who insists on being called 'the"
+                               " Dude,' a laid-back, easygoing burnout who"
+                               " happens to have the same name as a millionaire"
+                               " whose wife owes a lot of dangerous people a"
+                               " whole bunch of money -- resulting in the Dude"
+                               " having his rug soiled, sending him spiraling"
+                               " into the Los Angeles underworld.",
+                               "http://static.rogerebert.com/uploads/movie/movie_poster/the-big-lebowski-1998/large_aHaVjVoXeNanfwUwQ92SG7tosFM.jpg",
+                               "https://youtu.be/cd-go0oBF4Y")
+
+step_brothers = media.Movie("Step Brothers", "Brennan Huff (Will Ferrell) and"
+                            " Dale Doback (John C. Reilly) have one thing in"
+                            " common: they are both lazy, unemployed leeches"
+                            " who still live with their parents. When Brennan's"
+                            " mother and Dale's father marry and move in"
+                            " together, it turns the overgrown boys' world"
+                            " upside down. Their insane rivalry and narcissism"
+                            " pull the new family apart, forcing them to work"
+                            " together to reunite their parents.",
+                            "https://upload.wikimedia.org/wikipedia/en/d/d9/StepbrothersMP08.jpg",
+                            "https://youtu.be/8QKE96wZTkw")
+
+pulp_fiction = media.Movie("Pulp Fiction", "Vincent Vega (John Travolta) and"
+                           " Jules Winnfield (Samuel L. Jackson) are hitmen"
+                           " with a penchant for philosophical discussions. In"
+                           " this ultra-hip, multi-strand crime movie, their"
+                           " storyline is interwoven with those of their boss,"
+                           " gangster Marsellus Wallace (Ving Rhames) ; his"
+                           " actress wife, Mia (Uma Thurman) ; struggling boxer"
+                           " Butch Coolidge (Bruce Willis) ; master fixer"
+                           " Winston Wolfe (Harvey Keitel) and a nervous pair"
+                           " of armed robbers, 'Pumpkin' (Tim Roth) and 'Honey"
+                           " Bunny' (Amanda Plummer).",
+                           "https://unregardattentif.files.wordpress.com/2015/02/pulp_fiction-affiche.jpg",
+                           "https://youtu.be/ewlwcEBTvcg")
+
+movies = [role_models, fight_club, slc_punk, requiem_for_a_dream,
+          dirty_rotten_scoundrels, good_will_hunting, i_am_sam, death_proof,
+          the_grand_budapest_hotel, the_graduate, rocky_iv, memento,
+          the_big_lebowski, step_brothers, pulp_fiction]
+fresh_tomatoes.open_movies_page(movies)
+
